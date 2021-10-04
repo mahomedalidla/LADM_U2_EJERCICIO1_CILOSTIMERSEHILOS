5df5d3c92ec7697503e009c7d07cd3dd88f816f7
Mahomedalid Aguirre <mahomedalidla@gmail.com>
2021-10-04 09:26:20 -0600

    Initial commit

---
+++ app/src/main/java/mx/tecnm/tepic/ladm_u2_ejercicio1_cilostimersehilos/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.tecnm.tepic.ladm_u2_ejercicio1_cilostimersehilos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
